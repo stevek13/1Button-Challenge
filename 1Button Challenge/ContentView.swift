2f5202bba09a02c01d7d3afd5c4dfbe885f60cb2
Steve Kite <skite1@hotmail.com>
2026-01-08 07:41:58 -0800

    Initial Commit

---
+++ 1Button Challenge/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  1Button Challenge
+//
+//  Created by Steve on 1/8/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 1Button Challenge/_Button_ChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  _Button_ChallengeApp.swift
+//  1Button Challenge
+//
+//  Created by Steve on 1/8/26.
+//
+
+import SwiftUI
+
+@main
+struct _Button_ChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
