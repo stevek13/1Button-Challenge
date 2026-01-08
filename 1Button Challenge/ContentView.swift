0cca5297327b3ae885bc43e0aef3b4c392d193b5
Steve Kite <skite1@hotmail.com>
2026-01-08 09:04:20 -0800

    1Button Challenge from Prof. G's course

--- 1Button Challenge/ContentView.swift
+++ 1Button Challenge/ContentView.swift
@@ -8,17 +8,44 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var message = ""
+    @State private var imageName = ""
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Spacer()
+            Image(systemName: imageName)
+                .resizable()
+                .scaledToFit()
+                .foregroundStyle(.orange)
+                .padding()
+            Text(message)
+                .font(.largeTitle)
+                .fontWeight(.ultraLight)
+            Spacer()
+            
+            Button("Press Me!") {
+                let message1 = "You Are Awesom"
+                let message2 = "You Are Great!"
+                let image1 = "sun.max.fill"
+                let image2 = "hand.thumbsup"
+                
+                if message == message1 {
+                    message = message2
+                    imageName = image2
+                } else {
+                    message = message1
+                    imageName = image1
+                }
+                
+            }
+            .buttonStyle(.borderedProminent)
+            .font(.title2)
+            .tint(Color(.orange))
+            .padding()
         }
-        .padding()
     }
 }
-
 #Preview {
     ContentView()
 }
